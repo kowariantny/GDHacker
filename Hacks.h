7cb72eec6b8370d8a2d1ecb9e8581ccf404e9831
tqFan <19959666+kowariantny@users.noreply.github.com>
2021-01-28 01:03:15 +0100

    update to 1.1.9.0

--- Hacks.h
+++ Hacks.h
@@ -6,15 +6,17 @@ const char* const _ENGINE_MODULE_NAME = "Engine.dll";
 const char* const _GAME_MODULE_NAME = "Game.dll";
 
 const uintptr_t _GAME_SPEED_OFFSET = 0x416780;
-const uintptr_t _FREEZE_TRIBUTES_OFFSET = 0x37FFBF;
-const uintptr_t _PLAYSTATS_OFFSET = 0xF7BD;
-const uintptr_t _FREEZE_EXP_OFFSET = 0x1E4ECA;
 
+const uintptr_t _FREEZE_TRIBUTES_OFFSET = 0x38021F;
 const std::vector<unsigned char> _FREEZE_TRIBUTES_INJECT(6, 0x90);
+
+const uintptr_t _PLAYSTATS_OFFSET = 0xF7AD;
 const std::vector<unsigned char> _PLAYSTATS_INJECT[2] = {
     std::vector<unsigned char>{0x75, 0x1B},
     std::vector<unsigned char>(2, 0x90)
 };
+
+const uintptr_t _FREEZE_EXP_OFFSET = 0x1E503A;
 const std::vector<unsigned char> _FREEZE_EXP_INJECT[2] = {
     std::vector<unsigned char>{0x01, 0x51, 0x7C},
     std::vector<unsigned char>(3, 0x90)
