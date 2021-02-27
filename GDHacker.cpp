55da1fae0e0399e7f9dc0fb082e6d3c0268f9ce3
tqFan <19959666+kowariantny@users.noreply.github.com>
2021-02-28 00:19:39 +0100

    change program to FreezeTributes

--- GDHacker.cpp
+++ GDHacker.cpp
@@ -3,30 +3,7 @@
 
 int main(int argc, char** argv)
 {
-    if (argc < 2)
-        return 0;
-
-    switch (atoi(argv[1]))
-    {
-    case 0:
-        if (argc > 2)
-            ChangeSpeed(atof(argv[2]));
-        break;
-
-    case 1:
-        FreezeTributes();
-        break;
-
-    case 2:
-        if (argc > 2)
-            PlayStatsToggle(atoi(argv[2]));
-        break;
-
-    case 3:
-        if (argc > 2)
-            FreezeExp(atoi(argv[2]));
-        break;
-    }
+    FreezeTributes();
 }
 
 
