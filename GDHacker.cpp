60adf5d5fecb3cd833f49948f98cdb8d94d60740
tqFan <19959666+kowariantny@users.noreply.github.com>
2021-08-13 17:24:29 +0200

    update to 1.1.9.2

--- GDHacker.cpp
+++ GDHacker.cpp
@@ -3,7 +3,30 @@
 
 int main(int argc, char** argv)
 {
-    FreezeTributes();
+    if (argc < 2)
+        return 0;
+
+    switch (atoi(argv[1]))
+    {
+    case 0:
+        if (argc > 2)
+            ChangeSpeed(atof(argv[2]));
+        break;
+
+    case 1:
+        FreezeTributes();
+        break;
+
+    case 2:
+        if (argc > 2)
+            PlayStatsToggle(atoi(argv[2]));
+        break;
+
+    case 3:
+        if (argc > 2)
+            FreezeExp(atoi(argv[2]));
+        break;
+    }
 }
 
 
